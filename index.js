b1a84ce994260f1c5b953b0efa9085a72eec1624
rajshubham1999 <rajshubham834@gmail.com>
2024-02-09 01:21:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require('dotenv').config();
+const dbConfig = require("./config/dbConfig")
+const PORT = process.env.PORT || 3001 
+
+const userRoute = require("./routes/userRoutes");
+const movieRoutes = require("./routes/movieRoutes");
+const theatreRoutes = require("./routes/theatreRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+
+
+//app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    credentials: true // Allow sending credentials (cookies, authorization headers, etc.) with requests
+}));
+app.use(express.json());
+app.use('/api/user', userRoute);
+app.use("/api/movie", movieRoutes);
+app.use("/api/theatre", theatreRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
